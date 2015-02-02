7eb1f6c1a0fda76b26195e6913d04ed7518b3dc2
Yanislav Galyov <y.galyov@gmail.com>
2015-02-02 18:51:40 +0200

    Form is heavily modified for personal use

--- RogueSharp/Properties/AssemblyInfo.cs
+++ RogueSharp/Properties/AssemblyInfo.cs
@@ -1,18 +1,26 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("RogueSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RogueSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-[assembly: AssemblyTitle( "RogueSharp" )]
-[assembly: AssemblyDescription( "A library for roguelike developers to provide utilities frequently used in roguelikes. Inspired by libtcod" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Dreamers Design" )]
-[assembly: AssemblyProduct( "RogueSharp" )]
-[assembly: AssemblyCopyright( "Copyright © Faron Bracy 2014" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8da2242b-0b73-4553-ba5b-6b0277796151")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion( "1.2.0.0" )]
-[assembly: AssemblyFileVersion( "1.2.0.0" )]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
